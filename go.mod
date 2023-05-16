183d93891ed29817e7d3b2061fb5157f605d0ed4
Jan Stamer <jan.stamer@ppi.de>
2023-05-16 18:54:36 +0200

    Strip back to v0.1

--- go.mod
+++ go.mod
@@ -5,11 +5,7 @@ go 1.20
 require (
 	github.com/go-chi/chi/v5 v5.0.8
 	github.com/golang-migrate/migrate/v4 v4.15.2
-	github.com/google/uuid v1.3.0
-	github.com/hellofresh/health-go/v5 v5.1.1
-	github.com/jackc/pgx/v5 v5.3.1
 	github.com/kelseyhightower/envconfig v1.4.0
-	schneider.vip/problem v1.8.1
 )
 
 require (
@@ -24,11 +20,10 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgtype v1.14.0 // indirect
 	github.com/jackc/pgx/v4 v4.18.1 // indirect
-	github.com/jackc/puddle/v2 v2.2.0 // indirect
-	go.opentelemetry.io/otel v1.15.1 // indirect
-	go.opentelemetry.io/otel/trace v1.15.1 // indirect
+	github.com/lib/pq v1.10.6 // indirect
+	github.com/stretchr/testify v1.8.2 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/sync v0.2.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"context"
 	"embed"
 	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
-	"time"
 
 	"github.com/go-chi/chi/v5"
-	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/pgx"
-	"github.com/golang-migrate/migrate/v4/source/iofs"
-	"github.com/hellofresh/health-go/v5"
-	healthPgx "github.com/hellofresh/health-go/v5/checks/pgx4"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/kelseyhightower/envconfig"
-	"schneider.vip/problem"
 )
 
 //go:embed migrations
@@ -45,7 +35,7 @@ func HandleQuote(w http.ResponseWriter, r *http.Request) {
 	var quote Quote
 	err := json.NewDecoder(r.Body).Decode(&quote)
 	if err != nil {
-		problem.New(problem.Wrap(err), problem.Status(http.StatusBadRequest)).WriteTo(w)
+		http.Error(w, "Could not decode quote.😔", http.StatusBadRequest)
 		return
 	}
 
@@ -54,43 +44,8 @@ func HandleQuote(w http.ResponseWriter, r *http.Request) {
 
 	err = json.NewEncoder(w).Encode(quote)
 	if err != nil {
-		problem.New(problem.Wrap(err), problem.Status(http.StatusInternalServerError)).WriteTo(w)
+		http.Error(w, "Could not encode quote.😔", http.StatusBadRequest)
 	}
-}
-
-func Connect(dbURL string) (*pgxpool.Pool, error) {
-	err := Migrate(dbURL)
-	if err != nil {
-		return nil, err
-	}
-
-	connPool, err := pgxpool.New(context.Background(), dbURL)
-	if err != nil {
-		return nil, err
-	}
-
-	return connPool, nil
-}
-
-func Migrate(dbURL string) error {
-	source, err := iofs.New(migrations, "migrations")
-	if err != nil {
-		return err
-	}
-
-	dbMigrateURL := strings.Replace(dbURL, "postgres://", "pgx://", 1)
-	m, err := migrate.NewWithSourceInstance("iofs", source, dbMigrateURL)
-	if err != nil {
-		return err
-	}
-	defer m.Close()
-
-	err = m.Up()
-	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		return err
-	}
-
-	return nil
 }
 
 func main() {
@@ -100,35 +55,8 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
-	connPool, err := Connect(config.Db)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
 	r := chi.NewRouter()
 	r.Post("/api/quote", HandleQuote)
-
-	// CRUD API für Angebote
-	offerRepository := &OfferRepository{connPool: connPool}
-	r.Route("/api/offer", func(sr chi.Router) {
-		sr.Post("/", HandleCreateOffer(offerRepository))
-		sr.Get("/{ID}", HandleReadOffer(offerRepository))
-	})
-
-	// Register Health Check
-	h, _ := health.New(health.WithChecks(
-		health.Config{
-			Name:      "db",
-			Timeout:   time.Second * 2,
-			SkipOnErr: false,
-			Check: healthPgx.New(healthPgx.Config{
-				DSN: config.Db,
-			}),
-		},
-	))
-
-	// Register Health Check Handler Function
-	r.Get("/health", h.HandlerFunc)
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello DogOp!"))
